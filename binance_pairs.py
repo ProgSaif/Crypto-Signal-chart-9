42b57fbdad87563223235782d2e1c6ae4c83b940
ProgSaif <isaifulone@gmail.com>
2026-03-11 13:18:23 +0600

    Update binance_pairs.py

--- binance_pairs.py
+++ binance_pairs.py
@@ -1,14 +1,10 @@
 import requests
 
 def get_all_pairs():
-
     url = "https://api.binance.com/api/v3/exchangeInfo"
     data = requests.get(url).json()
-
     pairs = []
-
     for s in data["symbols"]:
         if s["quoteAsset"] == "USDT" and s["status"] == "TRADING":
             pairs.append(s["symbol"])
-
     return pairs
